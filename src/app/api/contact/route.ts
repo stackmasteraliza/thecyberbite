04f7a524fdb5189119b82ac52bc7738ae9eff2e5
stackmasteraliza <alizaali9490@gmail.com>
2026-05-04 16:36:03 +0500

    Changes

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -23,6 +23,7 @@ export async function POST(req: NextRequest) {
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
     secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
